efe33b1f760e224493ef53ab19b2455c902f38e8
Iroh-5 <58617376+Iroh-5@users.noreply.github.com>
2021-01-06 22:35:24 +0900

    Add files via upload

---
+++ timer.hpp
@@ -0,0 +1,35 @@
+#ifndef TIMER
+#define TIMER
+
+#include <chrono>
+
+template <typename Stream>
+class Timer
+{
+public:
+	Timer() = delete;
+
+	Timer(Stream& _str, const char* _name)
+		: prcName { _name }, str { _str }
+	{
+		m_StartPoint = std::chrono::high_resolution_clock::now();
+	}
+
+	~Timer()
+	{
+		auto endPoint = std::chrono::high_resolution_clock::now();
+
+		auto start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartPoint).time_since_epoch().count();
+		auto stop = std::chrono::time_point_cast<std::chrono::microseconds>(endPoint).time_since_epoch().count();
+		auto duration = stop - start;
+
+		str << prcName << " ended:" << std::endl;
+		str << std::fixed << duration << "(mcrs)\n" << duration * 1e-3 << "(ms)\n" << duration * 1e-6 << "(s)\n";
+	}
+private:
+	Stream& str;
+	std::chrono::time_point<std::chrono::high_resolution_clock> m_StartPoint;
+	const char* prcName;
+};
+
+#endif

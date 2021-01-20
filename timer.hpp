3747e0ffa450e63a977fb3980bc43b56bd61e5d9
Iroh-5 <58617376+Iroh-5@users.noreply.github.com>
2021-01-20 16:28:14 +0900

    Update timer.hpp

--- timer.hpp
+++ timer.hpp
@@ -7,20 +7,22 @@ template <typename Stream>
 class Timer
 {
 public:
+	using std::chrono;
+	
 	Timer() = delete; // default constructor is deleted
 
 	Timer(Stream& _str, const char* _name)
 		: prcName { _name }, str { _str }
 	{
-		m_StartPoint = std::chrono::high_resolution_clock::now();
+		m_StartPoint = high_resolution_clock::now();
 	}
 
 	~Timer()
 	{
-		auto endPoint = std::chrono::high_resolution_clock::now();
+		auto endPoint = high_resolution_clock::now();
 
-		auto start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartPoint).time_since_epoch().count();
-		auto stop = std::chrono::time_point_cast<std::chrono::microseconds>(endPoint).time_since_epoch().count();
+		auto start = time_point_cast<microseconds>(m_StartPoint).time_since_epoch().count();
+		auto stop = time_point_cast<microseconds>(endPoint).time_since_epoch().count();
 		auto duration = stop - start;
 
 		str << prcName << " ended:" << std::endl;
@@ -28,7 +30,7 @@ public:
 	}
 private:
 	Stream& str;
-	std::chrono::time_point<std::chrono::high_resolution_clock> m_StartPoint;
+	time_point<std::chrono::high_resolution_clock> m_StartPoint;
 	const char* prcName;
 };
 
